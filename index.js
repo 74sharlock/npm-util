4386a329597e162686d45673675b2d7bb1274ae7
74sharlock <2220106810@qq.com>
2016-07-25 12:40:59 +0800

    -init

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const path = require('path');
 const util = require('util');
 const fs = require('fs');
 
-let operate = function (opts, install) {
-    let wannerRemove = install === false;
+let operate = function (opts, command, handle) {
     opts = Object.assign({
         load: {
             prefix: process.cwd(),
@@ -21,39 +20,13 @@ let operate = function (opts, install) {
     opts.name && npm.load(opts.load, function (er) {
         if (er) return er;
 
-        npm.commands[wannerRemove ? 'uninstall' :'install']([opts.name], function (er, data) {
+        npm.commands[command]([opts.name], function (er, data) {
 
             if (er) {
                 return util.isFunction(opts.onError) && opts.onError.call(npm, er);
             }
 
-            if(opts.save || opts.saveDev){
-                let $pkgPath = path.resolve(opts.load.prefix + '/package.json');
-                require.cache[$pkgPath] && (delete require.cache[$pkgPath]);
-
-                let $pkg = require($pkgPath),
-                    key = opts.save ? 'dependencies' : 'devDependencies';
-
-                !$pkg[key] && ($pkg[key] = {});
-
-                if(wannerRemove){
-                    delete $pkg[key][opts.name];
-                } else {
-                    let packet = data[data.length-1][0].split('@'),
-                        packetName = packet[0],
-                        packetVersion = packet[1];
-
-                    $pkg[key][packetName] = opts.name.indexOf('@') < 0 ? `^${packetVersion}` : packetVersion;
-                }
-
-
-                fs.writeFile($pkgPath, JSON.stringify($pkg, null, 2), 'utf8', function (err) {
-                    if(err) return console.log(err);
-                    util.isFunction(opts.onComplete) && opts.onComplete.call(npm, data);
-                })
-            } else {
-                util.isFunction(opts.onComplete) && opts.onComplete.call(npm, data);
-            }
+            util.isFunction(handle) && handle(data);
 
         });
 
@@ -63,12 +36,51 @@ let operate = function (opts, install) {
     });
 };
 
+let useInstall = function (opts, data, wannerRemove) {
+    if(opts.save || opts.saveDev){
+        let $pkgPath = path.resolve(opts.load.prefix + '/package.json');
+        require.cache[$pkgPath] && (delete require.cache[$pkgPath]);
+
+        let $pkg = require($pkgPath),
+            key = opts.save ? 'dependencies' : 'devDependencies';
+
+        !$pkg[key] && ($pkg[key] = {});
+
+        if(wannerRemove){
+            delete $pkg[key][opts.name];
+        } else {
+            let packet = data[data.length-1][0].split('@'),
+                packetName = packet[0],
+                packetVersion = packet[1];
+
+            $pkg[key][packetName] = opts.name.indexOf('@') < 0 ? `^${packetVersion}` : packetVersion;
+        }
+
+
+        fs.writeFile($pkgPath, JSON.stringify($pkg, null, 2), 'utf8', function (err) {
+            if(err) return console.log(err);
+            util.isFunction(opts.onComplete) && opts.onComplete.call(npm, data);
+        })
+    } else {
+        util.isFunction(opts.onComplete) && opts.onComplete.call(npm, data);
+    }
+};
+
 module.exports = {
     parent: npm,
     install(opts){
-        operate(opts);
+        operate(opts, 'install', function (data) {
+            useInstall(opts, data)
+        });
     },
     uninstall(opts){
-        operate(opts, false);
+        operate(opts, 'uninstall', function (data) {
+            useInstall(opts, data, true);
+        });
+    },
+    run(opts){
+        operate(opts, 'run-script', function (data) {
+            util.isFunction(opts.onComplete) && opts.onComplete.call(npm, data);
+        });
     }
 };
